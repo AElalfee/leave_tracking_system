9e92141bc5c808a869213a0836a156f2c9c177d3
Abdelrahman Elalef <abdelrahman@elalfee.com>
2026-05-13 14:48:32 +0400

    Leave request class

---
+++ src/leave_tracking/LeaveRequest.java
@@ -0,0 +1,39 @@
+package leave_tracking;
+
+public class LeaveRequest {
+	private int requestId = 0;
+	private String startDate;
+	private String endDate;
+	private String status;
+	private String reason;
+	private Employee employee;
+
+	public LeaveRequest(String startDate, String endDate, String reason, Employee employee) {
+		requestId = requestId + 1;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.reason = reason;
+		this.employee = employee;
+	}
+	
+	public String getLeaveRange() {
+		return ("Starts from " + startDate + " Untill " + endDate);
+	}
+	
+	public String getStatus() {
+		return status;
+	}
+	
+	public String getReason() {
+		return reason;
+	}
+	
+	public Employee getEmployee() {
+		return employee;
+	}
+	
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+}
